87ac4cdf41c892c1da8ec64ebe4f318cecda4b9e
NadeemRais <77622962+NadeemRais@users.noreply.github.com>
2022-05-04 15:55:23 +0530

    Update ImageTag.groovy

--- vars/ImageTag.groovy
+++ vars/ImageTag.groovy
@@ -1,5 +1,9 @@
-  def version = readFile('VERSION')
-  def versions = version.split('\\.')
-  def major = versions[0]
-  def minor = versions[0] + '.' + versions[1]
-  def patch = version.trim()
+//   def version = readFile('VERSION')
+//   def versions = version.split('\\.')
+//   def major = versions[0]
+//   def minor = versions[0] + '.' + versions[1]
+//   def patch = version.trim()
+
+def call(name){
+  echo "hello ${name} how are you"
+}
