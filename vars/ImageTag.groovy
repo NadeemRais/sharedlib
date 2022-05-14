6977cd1e11ccea93dd2df1b039e5fc76e2754806
NadeemRais <77622962+NadeemRais@users.noreply.github.com>
2022-05-14 11:35:33 +0530

    Update ImageTag.groovy

--- vars/ImageTag.groovy
+++ vars/ImageTag.groovy
@@ -4,6 +4,35 @@
 //   def minor = versions[0] + '.' + versions[1]
 //   def patch = version.trim()
 
-def call(name){
-  echo "hello ${name} how are you"
+def call(ImageTag){
+    echo "${ImageTag}"
+    sh "curl -X GET -u '${NEXUS_USERNAME}':'${NEXUS_PASSWORD}' http://192.168.3.37:8081/repository/my-repo/VERSION -O"
+		version = sh returnStdout: true, script: "cat VERSION | grep '${application}' | grep -Eo '[0-9]+(.[0-9]+)*'"		
+		echo "$version"
+ 
+		   if("${ImageTag}"=="Patch"){
+                      echo "This is patch version"
+		      def versions = version.split('\\.')
+		      def last = versions[2]
+		      last = last.toInteger() + 1;
+		      new_version = versions[0] + '.' + versions[1]+'.'+"$last"
+		     
+		      
+		      }else if("${ImageTag}"=="Minor"){
+		      def versions = version.split('\\.')
+		      def mid = versions[1]
+		      mid = mid.toInteger() + 1;
+		      new_version = versions[0] + '.' + "$mid"+'.'+0
+		      echo "$new_version"
+		     
+		      
+		      }else{
+                      echo "This is major version"
+		      def versions = version.split('\\.')
+		      def first = versions[0]
+		      first = first.toInteger() + 1;
+		      new_version = "$first" + '.' + 0 +'.' + 0
+		      echo "$new_version"
+		    
+		    }     
 }
