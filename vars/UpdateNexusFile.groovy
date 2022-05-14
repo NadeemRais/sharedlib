273d3ece38d4a6cc16db3f642c37f24c7e853863
NadeemRais <77622962+NadeemRais@users.noreply.github.com>
2022-05-14 13:53:05 +0530

    Update UpdateNexusFile.groovy

--- vars/UpdateNexusFile.groovy
+++ vars/UpdateNexusFile.groovy
@@ -1,6 +1,6 @@
 def call(application,version,new_version){
   def app_ver = "${application}"+'='+"${version}".trim()
-	def new_app_ver = "${application}"+'='+"${new_version}".trim()
+  def new_app_ver = "${application}"+'='+"${new_version}".trim()
   sh "sed -i 's/${app_ver}/${new_app_ver}/g' VERSION" 
-  //sh "curl -v -u '${NEXUS_USERNAME}':'${NEXUS_PASSWORD}' --upload-file VERSION http://192.168.3.37:8081/repository/my-repo/"
+  sh "curl -v -u '${NEXUS_USERNAME}':'${NEXUS_PASSWORD}' --upload-file VERSION http://192.168.3.37:8081/repository/my-repo/"
 }
